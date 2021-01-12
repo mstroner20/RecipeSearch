50de313d804a7419d052a6218312c8bd00a95157
Michael Stroner <56314014+mstroner20@users.noreply.github.com>
2021-01-12 14:22:41 -0600

    Added more styles
    -Centering of the recipe div
    -Dynamically creates img tags along with any text tag
    
    TODO: add all the info from the recipe below the img
    allow user to select which ingredients
    improve styling of items

--- public/recipes.js
+++ public/recipes.js
@@ -1,8 +1,9 @@
+/*
 function getRecipesFromIngredients(){
 
 var data;
 
-fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=rice%2Cchicken%2Csteak&number=5&ranking=1&ignorePantry=true", {
+fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=milk%2Ceggs%2Clamb&number=5&ranking=1&ignorePantry=true", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "09fdfdf010msh9500f94e1190e33p19240cjsn26b193e09883",
@@ -31,10 +32,63 @@ fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findB
 
 function printData(data){
 
-    var elem = document.createElement("img");
-    elem.src = data[2].image;
-    console.log(data[2].image);
+    
 
-    document.getElementById("image").appendChild(elem);
+    for(i = 0; i < 5; i++){
+        var newDiv = document.createElement("div");
+        newDiv.id = "newImgDiv";
+        var elem = document.createElement("img");
+        var text = document.createElement("h3");
+        var lineBreak = document.createElement("br");
+        
+        elem.src = data[i].image;
+        text.textContent = "This is a bloody Test";
+        document.getElementById("recipeImages").appendChild(newDiv);
+        document.getElementById("newImgDiv").appendChild(elem);
+        document.getElementById("newImgDiv").appendChild(text);
+        document.getElementById("newImgDiv").appendChild(lineBreak);
+        
+        
+
+    }
+
+    
+    
+
+    
 
 }
+*/
+
+
+function getRecipesFromIngredients(){
+    var fake = "";
+    printData(fake);
+}
+function printData(fake){
+
+   
+
+    for(i = 0; i < 5; i++){
+        
+        var newDiv = document.createElement("div");
+        newDiv.id = "newImgDiv";
+        var elem = document.createElement("img");
+        var text = document.createElement("h3");
+        var lineBreak = document.createElement("br");
+        
+        elem.src = "dior.jpg"
+        text.textContent = "This is a bloody Test";
+        document.getElementById("recipeImages").appendChild(newDiv);
+        document.getElementById("newImgDiv").appendChild(elem);
+        document.getElementById("newImgDiv").appendChild(text);
+        document.getElementById("newImgDiv").appendChild(lineBreak);
+        
+        
+        
+        
+    
+    }
+
+}
+ 
